0413acaaa63737e8f9090d24d998928447d5a24a
VladWithCode <vladwithcode@gmail.com>
2024-10-08 10:51:16 -0600

    clean & use .gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,17 @@ module.exports = {
   theme: {
     extend: {
         colors: {
-            "main": "#798645",
-            "secondary": "#FEFAE0",
-            "light": "#F2EED7",
-            "accent": "#626F47",
+            "main": "#679436",
+            "secondary": "#427AA1",
+            "light": "#EBF2FA",
+            "accent": "#A5BE00",
+            /*
+            * --avocado: #6d9811ff;
+            * --aquamarine: #bcffe2ff;
+            * --light-green: #7be288ff;
+            * --pigment-green: #2a9e34ff;
+            * --forest-green: #319026ff;
+            */
         },
         fontFamily: {
             "main": "'Plus Jakarta Sans', sans-serif",
